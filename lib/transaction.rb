0d78899d8e1e61097fe4ca881e5d7c1a0416a377
majdeddine <majda-jambouri@hotmail.fr>
2018-01-02 18:01:33 +0000

    add type, amount, balance and date attributes to transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,6 +1,12 @@
-class Transaction
 
-  def initialize(type,amount,date)
+class Transaction
+  attr_reader :type,:date, :amount, :balance
+  def initialize(type,amount,balance,date=Time.now)
+    @type = type
+    @amount = amount
+    @balance = balance
+    @date = date
+
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,6 +1,20 @@
 describe Transaction do
+  let(:transaction){Transaction.new('credit',10,100)}
 
   it "have 3 attributes when instantiated"do
-    expect(Transaction).to respond_to(:new).with(3).arguments
+    expect(Transaction).to respond_to(:new).with(4).arguments
   end
+
+  it "have a type" do
+    expect(transaction.type).to eq('credit')
+  end
+
+  it "have an amount" do
+    expect(transaction.amount).to eq(10)
+  end
+
+  it "have a balance" do
+    expect(transaction.balance).to eq(100)
+  end
+
 end
