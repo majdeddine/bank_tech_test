81d1a0a8330ae48c4b3fc9117593810827dcd764
majdeddine <majda-jambouri@hotmail.fr>
2018-01-02 18:57:29 +0000

    add transaction to to transactions in the account

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,32 @@
 class Account
   attr_reader :balance, :owner, :password, :transactions
-  def initialize(customer, password)
+  def initialize(customer, password, transaction = Transaction)
     @owner = customer
     @password = password.to_s
     @balance = 0
+    @transaction = transaction
     @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    add_transaction('debit',amount,@balance)
   end
 
   def withdraw(amount)
     @balance -= amount
+    add_transaction('credit',amount,@balance)
+  end
+
+  private
+
+  def add_transaction(type,amount,balance)
+    transaction = create_transaction(type,amount,balance)
+    @transactions.push(transaction)
+  end
+
+  def create_transaction(type,amount,balance)
+    @transaction.new(type,amount,balance)
   end
 
 end
--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,7 +1,7 @@
 
 class Transaction
   attr_reader :type,:date, :amount, :balance
-  def initialize(type,amount,balance,date)
+  def initialize(type,amount,balance,date=Time)
     @type = type
     @amount = amount
     @balance = balance
