2101cbc18c9fe91b4fea3f8ca388555b3774439b
majdeddine <majda-jambouri@hotmail.fr>
2018-01-02 17:20:00 +0000

    throw error if passowrd is wrong when making a deposit

--- lib/bank.rb
+++ lib/bank.rb
@@ -11,8 +11,12 @@ class Bank
   end
 
   def deposit(amount, customer, password)
-    account = find_account(customer)
+    account = find_account(customer,password)
+    if account
     account.deposit(amount)
+    else
+      match_error
+    end
   end
 
   def withdraw(amount, customer,password)
@@ -26,8 +30,11 @@ class Bank
     @account.new(customer, password)
   end
 
-  def find_account(customer)
-    @accounts.select{|c| c.owner == customer }[0]
+  def find_account(customer,password)
+    @accounts.select{|c| c.owner == customer && c.password == password }[0]
   end
 
+  def match_error
+    raise("Wrong name or password")
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -12,7 +12,7 @@ describe Bank do
     end
 
     it"create a new account and add it to the accounts list"do
-      bank.new_account(customer,0000)
+      bank.new_account(customer,"0000")
       expect(bank.accounts.last.owner).to eq(customer)
     end
   end
@@ -20,6 +20,10 @@ describe Bank do
   describe "deposit" do
     it "receive amount, customer and password as arguments" do
       expect(bank).to respond_to(:deposit).with(3).argument
+    end
+
+    it "throw error if the customer provide a wrong password" do
+      expect{bank.deposit(10,'jj',"0001")}.to raise_error("Wrong name or password")
     end
   end
 
